1891a753f91047c6d48a82d7f518dbb8f284dc51
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-19 16:16:58 +0200

    adding meta information to the layout and also created the header and footer components

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,19 @@
 
-export default function Layout({children}) {
+import Head from "next/head"
+import Header from "./header"
+import Footer from "./footer"
+
+export default function Layout({children, title = '', description = ''}) {
   return (
     <>
+        <Head>
+            <title>{`GuitarLA - ${title}`}</title>
+            <meta name="description" content={description} />
+        </Head>
+        <Header/>
+        <h1>Desde Layout</h1>
         {children}
+        <Footer/>
     </>
   )
 }
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -2,7 +2,10 @@ import Link from 'next/link';
 import Layout from '@/components/layout';
 export default function Nosotros(){
     return(
-        <Layout>
+        <Layout
+            title={"Nosotros"}
+            description='Sobre nosotros, guitarLA, Tienda de musica'
+        >
             <Link href="/">Volver al Inicio</Link>
         </Layout>
     )
