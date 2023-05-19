934456fab46a1b6b19b518d308614d7e80950a29
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-19 17:19:44 +0200

    giving styles to header with module.css

--- components/header.js
+++ components/header.js
@@ -1,12 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from "../styles/header.module.css"
 
 export default function Header() {
   return (
-    <header>
-        <div className="contenedor">
+    <header className={styles.header}>
+        <div className={`contenedor ${styles.barra}`}>
             <Image src="/img/logo.svg" width={300} height={40} alt="logo header guitarLA"/>
-            <nav>
+            <nav className={styles.navegacion}>
                 <Link href="/">
                     Inicio
                 </Link>
