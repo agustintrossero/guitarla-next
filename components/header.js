67a0c84f4ba6e29c3b6e9b00f9720f787c5b0c91
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-19 18:03:23 +0200

    finished styling header component and footer aswell with module.css

--- components/header.js
+++ components/header.js
@@ -1,24 +1,38 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from "../styles/header.module.css"
 
 export default function Header() {
+
+    const router = useRouter()
+    //console.log(router)
   return (
     <header className={styles.header}>
         <div className={`contenedor ${styles.barra}`}>
-            <Image src="/img/logo.svg" width={300} height={40} alt="logo header guitarLA"/>
+            <Link legacyBehavior href="/">
+                <Image src="/img/logo.svg" width={300} height={40} alt="logo header guitarLA"/>
+            </Link>
             <nav className={styles.navegacion}>
-                <Link href="/">
-                    Inicio
+                <Link legacyBehavior href="/">
+                    <a className={router.pathname === "/" ? styles.active : ""}>
+                        Inicio
+                    </a>
                 </Link>
-                <Link href="/nosotros">
-                    Nosotros
+                <Link legacyBehavior href="/nosotros">
+                    <a className={router.pathname === "/nosotros" ? styles.active : ""}>
+                        Nosotros
+                    </a>
                 </Link>
-                <Link href="/blog">
-                    Blog
+                <Link legacyBehavior href="/blog">
+                    <a className={router.pathname === "/blog" ? styles.active : ""}>
+                        Blog
+                    </a>
                 </Link>
-                <Link href="/tienda">
-                    Tienda
+                <Link legacyBehavior href="/tienda">
+                    <a className={router.pathname === "/tienda" ? styles.active : ""}>
+                        Tienda
+                    </a>
                 </Link>
             </nav>
         </div>
