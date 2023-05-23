9951ed5c5e7fb85c84434ba62ba056b967671a45
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-23 20:47:57 +0200

    making the structure to read a single guitar and his url in a dynamic way, using getServerSideProps and using filter on its [url], there are several console.log to lear how to read the object/array data and how information is build from thee API request

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,19 +1,20 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from '../styles/guitarras.module.css'
 
 export default function Guitarra({guitarra}) {
   //console.log(guitarra)
   const {nombre, descripcion, precio, imagen, URL} = guitarra
 
   return (
-    <div>
+    <div className={styles.guitarra}>
       <Image src={`http://127.0.0.1:1337${imagen?.data?.attributes?.formats?.medium.url}`} width={600} height={400} alt={`Imagen de Guitarra ${nombre}`}/>
-      <div>
+      <div className={styles.contenido}>
         <h3>{nombre}</h3>
-        <p>{descripcion}</p>
-        <p>${precio}</p>
-        <Link href={`/guitarras/${URL}`}>
-          Ver producto
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
+        <Link legacyBehavior href={`/guitarras/${URL}`}>
+          <a className={styles.enlace}>Ver Producto</a>
         </Link>
       </div>
     </div>
