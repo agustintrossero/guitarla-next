5176f6b686c5b40b68ef0a932d960c0a02f41d65
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-24 15:05:35 +0200

    making dynamic routing using getStaticPaths and getStaticProps async functions to show single guitar information

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,18 +1,51 @@
-import { useRouter } from "next/router"
+import Image from "next/image"
+import styles from '../../styles/guitarras.module.css'
+import Layout from "@/components/layout"
 
-export default function Producto(guitarra) {
-    //const router = useRouter()
-    //console.log(router)
-    //console.log(guitarra.guitarra[0].attributes)
-
+export default function Producto({guitarra}) {
+const { nombre, descripcion, imagen, precio} = guitarra[0].attributes
   return (
-    <div>
-      URL
-    </div>
+    <Layout
+        title={`Guitarra: ${nombre}`}
+    >
+            <div className={styles.guitarra}>
+                <Image src={`http://127.0.0.1:1337${imagen?.data?.attributes.url}`} width={600} height={400} alt={`Imagen de Guitarra ${nombre}`}/>
+                <div className={styles.contenido}>
+                    <h3>{nombre}</h3>
+                    <p className={styles.descripcion}>{descripcion}</p>
+                    <p className={styles.precio}>${precio}</p>
+                </div>
+            </div>
+        </Layout>
   )
 }
 
-export async function getServerSideProps({query: {url}}){
+export async function getStaticPaths(){
+    const respuesta = await fetch(`${process.env.API_URL}/api/guitarras`)
+    const {data} = await respuesta.json()
+    const paths = data.map(guitarra => ({
+        params: {
+            url : String(guitarra.attributes.URL)
+        }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+ 
+export async function getStaticProps({params: {url}}) {
+ 
+    const respuesta = await fetch(`${process.env.API_URL}/api/guitarras?filters[url]=${url}&populate=imagen`)
+    const {data: guitarra} = await respuesta.json()
+    return {
+        props: {
+            guitarra
+        }
+    }
+}
+
+/* export async function getServerSideProps({query: {url}}){
     //console.log(datos)
     //console.log(url)
 const respuesta = await fetch(`${process.env.API_URL}/api/guitarras?filters[url]=${url}&populate=imagen`)
@@ -23,4 +56,4 @@ const {data: guitarra} = await respuesta.json()
             guitarra
         }
     }
-}
+} */
