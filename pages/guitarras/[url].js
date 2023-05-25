2c58e1a53237bcf8b251c75122d6550b7b712cf5
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-25 17:18:02 +0200

    added a form to select number of products and give some style

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -14,6 +14,18 @@ const { nombre, descripcion, imagen, precio} = guitarra[0].attributes
                     <h3>{nombre}</h3>
                     <p className={styles.descripcion}>{descripcion}</p>
                     <p className={styles.precio}>${precio}</p>
+                    <form className={styles.formulario}>
+                        <label htmlFor="cantidad">Cantidad:</label>
+                        <select id="cantidad">
+                            <option value="0">-- Seleccione --</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        <input type="submit" value="Agregar al carrito"/>
+                    </form>
                 </div>
             </div>
         </Layout>
@@ -44,16 +56,3 @@ export async function getStaticProps({params: {url}}) {
         }
     }
 }
-
-/* export async function getServerSideProps({query: {url}}){
-    //console.log(datos)
-    //console.log(url)
-const respuesta = await fetch(`${process.env.API_URL}/api/guitarras?filters[url]=${url}&populate=imagen`)
-const {data: guitarra} = await respuesta.json()
-//console.log(data)
-    return{
-        props:{
-            guitarra
-        }
-    }
-} */
