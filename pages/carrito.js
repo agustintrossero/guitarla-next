21c127691ac16905d25c250c2a5e21146ac660d1
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-26 12:17:53 +0200

    The functionality was added to select and update the number of products selected from the cart page, styles were also given to the selector and the cart update function brought from _app.js was applied.

--- pages/carrito.js
+++ pages/carrito.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Layout from "@/components/layout"
 import styles from "../styles/carrito.module.css"
 
-export default function Carrito({carrito}) {
+export default function Carrito({carrito, actualizarCantidad}) {
   //console.log(carrito)
   return (
     <Layout title="Carrito de compras">
@@ -19,7 +19,22 @@ export default function Carrito({carrito}) {
                       </div>
                       <div>
                         <p className={styles.nombre}>{producto.nombre}</p>
-                        <p>Cantidad: {producto.cantidad}</p>
+                        <div className={styles.cantidad}>
+                          <p>Cantidad: </p>
+                          <select 
+                          onChange={e => actualizarCantidad({
+                            id: producto.id,
+                            cantidad: e.target.value
+                          })}
+                          value={producto.cantidad}
+                          className={styles.select}>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
                         <p className={styles.precio}>$<span>{producto.precio}</span></p>
                         <p className={styles.subtotal}>Subtotal: $<span>{producto.cantidad * producto.precio}</span></p>
 
