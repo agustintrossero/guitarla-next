032366e91e26e30bfe720b14d15084cdf04ca901
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-26 18:03:07 +0200

    adding localStorage to cart and fixed hydration issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  
-  const [carrito, setCarrito] = useState([])
 
+export default function App({ Component, pageProps }) {
+  const carritoLS = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('carrito')) ?? [] : []
+  const [carrito, setCarrito] = useState(carritoLS)
+  const [paginaLista, setPaginaLista] = useState(false)
+ 
+  useEffect(()=> {
+    setPaginaLista(true)
+  }, [])
+ 
+  useEffect(()=> {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+  },[carrito])
   
   const agregarCarrito = guitarra => {
     // Comprobar si la guitarra ya esta en el carrito...
@@ -43,10 +52,10 @@ const actualizarCantidad = guitarra => {
   window.localStorage.setItem('carrito', JSON.stringify( carrito ));
 }
   
-  return <Component {...pageProps} 
+  return  paginaLista ? <Component {...pageProps} 
     carrito={carrito}
     agregarCarrito ={agregarCarrito}
     eliminarProducto={eliminarProducto}
     actualizarCantidad={actualizarCantidad}
-  />
+  />  : null 
 }
